77b86ff3881349ed6f3cd230a161fae0b6834575
Sunnatbek <Sunnatbek912@gmail.com>
2026-02-07 12:12:06 +0500

    doen

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './App.css'
 import Header from './Components/Header'
 import Input from './Components/Input'
 import Movie from './Components/Movie'
-import card1 from './assets/card1'
+import card1 from './assets/card1.jpg'
+import card2 from './assets/card2.jpg'
+import card3 from './assets/card3.jpg'
+import card4 from './assets/card4.jpg'
+import card5 from './assets/card5.jpg'
+import card7 from './assets/card7.jpg'
+import card8 from './assets/card8.jpg'
+
 
 function App() {
 
@@ -11,7 +18,41 @@ function App() {
      <Header/>
      <br />
      <Input/>
-     <Movie img={card1}/>
+     <Movie 
+     img={card1}
+     name={"MineCraft"}
+     year={"(2025)"}
+     />
+    <Movie 
+     img={card2}
+     name={"Avatar"}
+     year={"(2013)"}
+     />     
+     <Movie 
+     img={card3}
+     name={"Romeo"}
+     year={"(2009)"}
+     />    
+     <Movie 
+     img={card4}
+     name={"Joker"}
+     year={"(2016)"}
+     />   
+     <Movie 
+     img={card5}
+     name={"Avangards"}
+     year={"(2017)"}
+     />     
+     <Movie 
+     img={card7}
+     name={"Transformers"}
+     year={"(2015)"}
+     />     
+     <Movie 
+     img={card8}
+     name={"Alladdin"}
+     year={"(2023)"}
+     />
     </>
   )
 }
--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,10 +1,13 @@
 function Movie({ img, name, year }) {
 	return(
+		<div className="movie">
+
 		<div className="card">
 			<img src={img} alt="img" className="card-img" />
 			<h3 className='card-h3
 			'> {name} </h3>
 			<p className='card-p'><span className='secondary'></span> { year } </p>
+		</div>
 		</div>
 	)
 }
