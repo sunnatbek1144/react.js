ebc4c5c9c5456d39cde6bc8bbb6b2f41ac8df8b3
Sunnatbek <Sunnatbek912@gmail.com>
2026-02-19 17:58:49 +0500

    pochti gotof

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,1 +1,32 @@
 import { Link } from 'react-router';
+
+const Navbar = () => {
+	return (
+		<>
+		<nav>
+			<div className="logo">A_SHOP</div>
+			<ul>
+				<li>
+					<link to="/">Home</link>
+				</li>
+				<li>
+					<link to="/movies">Movies</link>
+				</li>
+				<li>
+					<link to="/Cart">Cart</link>
+				</li>
+				<li>
+					<link to="/Contact">Contact</link>
+				</li>
+				<li>
+					<link to="/Payment">Payment</link>
+				</li>
+				<li>
+					<link to="/Shop">Shop</link>
+				</li>
+			</ul>
+		</nav>
+		</>
+	)
+}
+export default Navbar;
