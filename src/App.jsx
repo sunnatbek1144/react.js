aea2849f38926db01c58d3ff6a2137f3d30d7488
Sunnatbek <Sunnatbek912@gmail.com>
2026-02-19 12:02:26 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Input from './Components/Input'
 import Movie from './Components/Movie'
 import card1 from './assets/card1.jpg'
 import card2 from './assets/card2.jpg'
-import card3 from './assets/card3.jpg'
 import card4 from './assets/card4.jpg'
 import card5 from './assets/card5.jpg'
-import card7 from './assets/card7.jpg'
 import card8 from './assets/card8.jpg'
 
 
@@ -31,11 +29,6 @@ function App() {
      year={"(2013)"}
      />     
      <Movie 
-     img={card3}
-     name={"Romeo"}
-     year={"(2009)"}
-     />    
-     <Movie 
      img={card4}
      name={"Joker"}
      year={"(2016)"}
@@ -44,11 +37,6 @@ function App() {
      img={card5}
      name={"Avangards"}
      year={"(2017)"}
-     />     
-     <Movie 
-     img={card7}
-     name={"Transformers"}
-     year={"(2015)"}
      />     
      <Movie 
      img={card8}
